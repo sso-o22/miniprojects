16790e485da7eee637b18980528503ca099cd228
sso-o22 <sso-o22@naver.com>
2023-05-08 17:19:55 +0900

    team

---
+++ part2/team/StudentCard/StudentCard/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentCard
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnID_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPW_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ part2/team/StudentCard/StudentCard/StudentManagement.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentCard
+{
+    /// <summary>
+    /// StudentManagement.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class StudentManagement : MetroWindow
+    {
+        public StudentManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExitProgram_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
